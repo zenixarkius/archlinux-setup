de836b89289c5c3b4fc5c96fc510e33174600999
zenixark <224643328+zenixarkius@users.noreply.github.com>
2025-08-13 17:14:05 -0400

    Redo the install.sh comments into nice blocks

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-########################################################
-# 1. PREPARATION FOR INSTALL
-########################################################
+###############################################################################
+# PREPARATION FOR INSTALL
+# 1. Convert arguments to variables
+# 2. Prompt the user for basic safety
+# 3. Setup Wi-Fi and timezone
+# 4. Check if this is a repeat attempt and unmount affected partitions
+###############################################################################
 
-# --- Setup customized variables ---
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -b) BOOT_PART="$2"; shift ;;
@@ -24,106 +27,123 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# --- Basic safety prompt ---
 read -rp $'\e[31mThis script will wipe everything and reformat. Type "IK" to continue: \e[0m' confirm
 if [[ "$confirm" != "IK" ]]; then
   echo -e "\e[31mAborting.\e[0m"
   exit 1
 fi
 
-# --- Check for mounted partitions to make repeated attempts less obnoxious ---
+if ! ping -c 1 archlinux.org &>/dev/null; then
+    iwctl --passphrase $WIFI_PASS station $WIFI_INT connect $WIFI_NAME
+fi
+
+timedatectl set-timezone America/New_York
+
 if mountpoint -q /mnt; then
     umount -R /mnt
     cryptsetup close cryptroot
 fi
 
-# --- Connect to the internet ---
-if ! ping -c 1 archlinux.org &>/dev/null; then
-    iwctl --passphrase $WIFI_PASS station $WIFI_INT connect $WIFI_NAME
-fi
 
-# --- Set the Live ISO timezone ---
-timedatectl set-timezone America/New_York
+###############################################################################
+# DISK CONFIGURATION
+# 1. Format the esp partition with FAT32
+# 2. Create a hardened LUKS-encrypted container on the root partition
+# 3. Format the container with Btrfs
+#    - No subvolumes because I just don't like them and I don't use snapshots
+# 4. Mount the formatted partitions with optimized settings
+#    - Defaults are already optimized and noatime is free performance
+#    - `compress-force=zstd:3` shrinks my base install from 18GB to 4.5GB!!
+###############################################################################
 
-
-########################################################
-# 2. DISK CONFIGURATION
-########################################################
-
-# --- Format the boot partition with FAT32 ---
 mkfs.fat -F 32 "/dev/${BOOT_PART}"
 
-# --- Create a LUKS-encrypted container on the root partition and format it with Btrfs ---
 echo $USER_PASS | cryptsetup -q luksFormat -h sha512 -i 5000 -s 512 "/dev/${ROOT_PART}"
 echo $USER_PASS | cryptsetup open "/dev/${ROOT_PART}" cryptroot -
+
 mkfs.btrfs -f /dev/mapper/cryptroot
 
-# --- Mount the partitions with optimized filesystem settings
 mount -o defaults,noatime,compress-force=zstd:3 /dev/mapper/cryptroot /mnt
 mkdir -p /mnt/boot
 mount -o defaults,noatime "/dev/${BOOT_PART}" /mnt/boot
 
 
-########################################################
-# 3. BOOTSTRAPPING THE NEW SYSTEM
-########################################################
+###############################################################################
+# BOOTSTRAPPING THE NEW SYSTEM
+# 1. Optimize pacman DL speed with an updated mirrorlist parallel downloads
+# 2. Sync the package database and keyring as it can be dated in the live ISO
+# 3. Install the base system and low level components into the new install
+# 4. Generated the Filesystem Table
+# 5. Stage my dotfiles into the new install before chrooting into it
+###############################################################################
 
-# --- Optimizing pacman download speed ---
 reflector -c US -p https -a 12 -l 20 -f 5 --sort rate --save /etc/pacman.d/mirrorlist
 sed -i 's/^#\(ParallelDownloads = 5\)/\1/' /etc/pacman.conf
 
-# --- Sync the package database and keyring then pacstrap the system ---
 pacman -Sy
 pacman -S --noconfirm archlinux-keyring
-pacstrap -K /mnt base base-devel btrfs-progs intel-ucode iwd linux linux-firmware nvidia pipewire-pulse sbctl
 
-# --- Generate the fstab ---
+pacstrap -K /mnt base base-devel intel-ucode linux linux-firmware nvidia sbctl
+
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# --- Prepare for chroot by staging dotfiles ---
 mkdir -p /mnt/dottmp
 cp $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/dotfiles/.bashrc /mnt/dottmp
 cp -r $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/dotfiles/.config/hypr /mnt/dottmp/hypr
 arch-chroot /mnt /bin/bash <<CHROOT
 
 
-########################################################
-# 4. SETTING UP THE NEW SYSTEM
-########################################################
+###############################################################################
+# SETTING UP THE NEW SYSTEM
+# 1. Synchronize the system and hardware clocks with my timezone
+# 2. Make resolv.conf a stub so that its managed by systemd-resolved
+# 3. Generate the locale
+# 4. Set the hostname to something generic
+# 5. Set the root password then disable root for extra security
+# 6. Create the new user with a generic name
+# 7. Symlink .cache to tmp to wipe it on shutdown
+# 8. Move staged dotfiles from earlier into their proper locations
+###############################################################################
 
-# --- Set up system clock ---
 ln -sf "/usr/share/zoneinfo/America/New_York" /etc/localtime
 hwclock --systohc
 timedatectl set-ntp true
 
-# --- Make resolv.conf managed by systemd-resolved ---
 rm /etc/resolv.conf
 ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
-# --- Generate locale ---
 sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# --- Set the hostname ---
 echo archlinux > /etc/hostname
 
-# --- Set the root password and disable root for security ---
 echo "root:$USER_PASS" | chpasswd
 passwd --lock root
 
-# --- Create the new user ---
 useradd -m user
 echo "user:$USER_PASS" | chpasswd
 
-# --- Symlink .cache to tmp for cleanliness ---
 ln -s /tmp /home/user/.cache
 
-# --- Setup kernel options including NVIDIA ones for performance ---
+mv /dottmp/.bashrc /home/user
+mv /dottmp/hypr /home/user/.config
+rmdir /dottmp
+
+
+###############################################################################
+# SETTING UP THE BOOT PROCESS
+# 1. Setup kernel options, including NVIDIA ones for gaming performance
+# 2. Create a UKI preset so that I don't need bootloader overhead
+#    - UKI's securitywise synergize well with Secure Boot and disk encryption
+#      This is currently the only way to prevent initramfs tampering
+# 3. Add encrypt to mkinitcpio hooks and regenerate the initramfs
+# 4. Setup secure boot keys and sign the UKI
+###############################################################################
+
 mkdir -p /etc/kernel
 echo "cryptdevice=UUID=$(blkid -s UUID -o value /dev/"${ROOT_PART}"):cryptroot root=/dev/mapper/cryptroot rw nvidia.NVreg_EnableGpuFirmware=0 nvidia.NVreg_UsePageAttributeTable=1" > /etc/kernel/cmdline
 
-# --- Create a UKI preset so that 1. I dont need a bootloader and 2. because it synergizes securitywise with Secure Boot ---
 cat <<EOF > /etc/mkinitcpio.d/linux.preset
 ALL_config="/etc/mkinitcpio.conf"
 ALL_kver="/boot/vmlinuz-linux"
@@ -133,52 +153,60 @@ default_uki="/boot/EFI/BOOT/BOOTX64.EFI"
 default_options="--cmdline /etc/kernel/cmdline"
 EOF
 
-# --- Add encrypt to mkinitcpio hooks and regenerate the initramfs ---
 sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
 mkdir -p /boot/EFI/BOOT
 mkinitcpio -p linux
 
-# --- Configure Secure Boot ---
 if sbctl status | grep -q "Setup Mode:     ✘ Enabled"; then
     sbctl create-keys
     sbctl enroll-keys --microsoft
     sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
 fi
 
-# --- Temporarily allow the user to use passwordless sudo for package installation ---
+
+###############################################################################
+# SETTING UP THE USER PACKAGES
+# 1. Temporarily allow the user to use passwordless sudo for yay
+# 2. Install yay and then my preffered packages
+# 3. Annihilate the orphans and build files
+# 4. Set depended-upon packages to dependency status
+# 5. Revoke passwordless sudo form the user for security
+###############################################################################
+
 echo "user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/00_user
 chmod 440 /etc/sudoers.d/00_user
 
-# --- Install yay and my preferred packages ---
 pacman -S --noconfirm --needed git
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
 runuser -l user -c 'makepkg -si --noconfirm'
-runuser -l user -c 'yay -S --noconfirm --needed hyprland hyprpaper hyprshot keepassxc librewolf-bin mullvad-vpn-cli noto-fonts openrgb pipewire-jack python-nvidia-ml-py vscodium-bin'
+runuser -l user -c 'yay -S --noconfirm --needed btrfs-progs hyprland hyprpaper hyprshot iwd keepassxc librewolf-bin mullvad-vpn-cli noto-fonts openrgb pipewire-jack pipewire-pulse python-nvidia-ml-py vscodium-bin'
 
-# --- Clean up orphans and build files and set depended-on packages to dep status ---
 pacman -Rcns --noconfirm $(pacman -Qttdq)
 pacman -Yc --noconfirm
-pacman -D --asdeps git noto-fonts pipewire-jack
 rm -rf /home/user/.cargo
 rm -rf /home/user/.config/go
 
-# --- Revoke passwordless sudo from the user for security ---
+pacman -D --asdeps git noto-fonts pipewire-jack
+
 echo "user ALL=(ALL) ALL" > /etc/sudoers.d/00_user
 chmod 440 /etc/sudoers.d/00_user
 
 
-########################################################
-# 5. DOTFILES AND CONFIGS
-########################################################
+###############################################################################
+# DOTFILES AND CONFIGS
+# 1. Create a bash profile to automatically start hyprland on sign in
+# 2. Configure getty to autologin the user for convenience
+# 3. Create my custom systemd service file
+# 4. Pre-configure my preferred VSCodium environment
+# 5. Fix .pulse-cookie bug with Steam
+# 6. Disable coredumps as theyre HUGE and I don't care about them
+# 7. Pre-configure my NextDNS profile via resolved
+# 8. Pre-configure Mullvad VPN with hardened settings
+# 9. Pre-configure Git
+###############################################################################
 
-# --- Move staged dotfiles into their proper locations ---
-mv /dottmp/.bashrc /home/user
-mv /dottmp/hypr /home/user/.config
-rmdir /dottmp
-
-# --- Create a bash profile to automatically start hyprland on sign in ---
 cat <<'HYPR' > /home/user/.bash_profile
 #
 # ~/.bash_profile
@@ -191,7 +219,6 @@ if [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then
 fi
 HYPR
 
-# --- Configure to autologin the user for convenience ---
 mkdir -p /etc/systemd/system/getty@tty1.service.d
 cat <<'GETTY' > /etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
@@ -199,7 +226,6 @@ ExecStart=
 ExecStart=-/sbin/agetty -o '-- \\u' --autologin user --noreset --noclear - \${TERM}
 GETTY
 
-# --- Create my custom systemd service file---
 cat <<CUSTOM > /etc/systemd/system/custom.service
 [Unit]
 Description=Setup NVIDIA overclocks, OpenRGB lighting, and fix my DNS resolver tug-of-war between resolved and Mullvad thanks to my barebones network stack.
@@ -217,7 +243,6 @@ while ! resolvectl status wg0-mullvad | grep -q "DNS Domain:"; do sleep 0.5; don
 WantedBy=multi-user.target
 CUSTOM
 
-# --- Configure my preferred VSCodium environment ---
 mkdir -p /home/user/.config/VSCodium/User
 cat <<VSCODIUM > /home/user/.config/VSCodium/User/settings.json
 {
@@ -233,50 +258,46 @@ cat <<VSCODIUM > /home/user/.config/VSCodium/User/settings.json
 }
 VSCODIUM
 
-# --- Fix .pulse-cookie bug with Steam ---
 sed -i 's|^; cookie-file =.*|cookie-file = ~/.config/pulse/cookie|' /etc/pulse/client.conf
 
-# --- Disable coredumps as theyre HUGE and I don't care about them ---
 sed -i 's/^#Storage=.*/Storage=none/' /etc/systemd/coredump.conf
 sed -i 's/^#ProcessSizeMax=.*/ProcessSizeMax=0/' /etc/systemd/coredump.conf
 
-# --- Configure my NextDNS profile via resolved ---
 sed -i "s|^#DNS=.*|DNS=45.90.28.0#${NEXTDNS}.dns.nextdns.io|" /etc/systemd/resolved.conf
 sed -i "/^DNS=45.90.28.0#/a DNS=2a07:a8c0::#${NEXTDNS}.dns.nextdns.io\nDNS=45.90.30.0#${NEXTDNS}.dns.nextdns.io\nDNS=2a07:a8c1::#${NEXTDNS}.dns.nextdns.io" /etc/systemd/resolved.conf
 sed -i 's/^#FallbackDNS=.*/FallbackDNS=/' /etc/systemd/resolved.conf
 sed -i 's/^#Domains=.*/Domains=~/' /etc/systemd/resolved.conf
 sed -i 's/^#DNSOverTLS=.*/DNSOverTLS=yes/' /etc/systemd/resolved.conf
 
-# --- Configure Mullvad VPN ---
 mullvad account login $MULLVAD
 mullvad relay set location any
 mullvad auto-connect set on
 mullvad lockdown-mode set on
 
-# --- Configure Git ---
 runuser -l user -c 'git config --global user.name "$GIT_USER"'
 runuser -l user -c 'git config --global user.email "$GIT_MAIL"'
 runuser -l user -c 'git config --global user.signingkey $GIT_KEY'
 runuser -l user -c 'git config --global commit.gpgSign true'
 
 
-########################################################
-# 6. WRAPPING UP THE INSTALL
-########################################################
+###############################################################################
+# WRAPPING UP THE INSTALL
+# 1. Ensure the user owns their own home
+# 2. Disable NVIDA services irrelevant to my desktop
+# 3. Enable filesystem maintainence timers
+# 4. Enable essential networking services
+# 5. Exit chroot, unmount partitions, and reboot into the new install
+###############################################################################
 
-# --- Ensure the user owns the files in their own home ---
 chown -R user:user /home/user
 
-# --- Disable useless NVIDIA laptop nonsense ---
 systemctl disable nvidia-hibernate
 systemctl disable nvidia-resume
 systemctl disable nvidia-suspend
 
-# --- Enable filesystem timers ---
 systemctl enable fstrim.timer
 systemctl enable btrfs-scrub@-.timer
 
-# --- Enable essential services ---
 systemctl enable custom
 systemctl enable iwd
 systemctl enable iptables
@@ -284,7 +305,6 @@ systemctl enable mullvad-daemon
 systemctl enable systemd-resolved
 systemctl enable systemd-timesyncd
 
-# --- Exit and reboot into the new install ---
 CHROOT
 umount -R /mnt
 cryptsetup close cryptroot
